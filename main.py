af82b6aa92ec1633a9b4c16e78addac06429351e
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2022-12-31 12:49:29 +0200

    add root page / add route requirements and read /requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from flask import Flask, Response, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/requirements/')
+def requirements():
+    with open('requirements.txt', 'r') as f:
+        content = f.read()
+    resp = Response(content, mimetype='text/plain')
+    return resp
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
