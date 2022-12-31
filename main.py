6e7c2a65e670fd09832d30a67890e68fcb9310d8
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2022-12-31 13:10:05 +0200

    add user generator list

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request, abort
+from faker import Faker
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +14,15 @@ def requirements():
         content = f.read()
     resp = Response(content, mimetype='text/plain')
     return resp
+
+fake = Faker()
+@app.route('/generate-users/')
+def generate_users():
+    count = request.args.get('count', default=100, type=int)
+    if count > 10000:
+        abort(400, 'GET par: count max >= 9999')
+    users = [fake.name() + ' ' + fake.email() for _ in range(count)]
+    return render_template('generate_users.html', users=users)
 
 if __name__ == '__main__':
     app.debug = True
