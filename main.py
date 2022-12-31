cd3dba8477332b52abbf86c53baf35ef015adf3b
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2022-12-31 18:15:49 +0200

    csv avg_height + avg_weight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import csv
 from flask import Flask, Response, render_template, request, abort
 from faker import Faker
 
@@ -23,6 +24,21 @@ def generate_users():
         abort(400, 'GET par: count max >= 9999')
     users = [fake.name() + ' ' + fake.email() for _ in range(count)]
     return render_template('generate_users.html', users=users)
+
+@app.route('/mean/')
+def mean():
+    heights = []
+    weights = []
+    with open('hw.csv', 'r') as f:
+        reader = csv.DictReader(f, fieldnames=["Index", "Height(Inches)", "Weight(Pounds)"])
+        next(reader)
+        for row in reader:
+            heights.append(float(row['Height(Inches)']))
+            weights.append(float(row['Weight(Pounds)']))
+    avg_height = sum(heights) / len(heights)
+    avg_weight = sum(weights) / len(weights)
+    return render_template('mean.html', avg_height=avg_height, avg_weight=avg_weight)
+
 
 if __name__ == '__main__':
     app.debug = True
